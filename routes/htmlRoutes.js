dc5d26d04082bf2d88e87c5cd2b585e407c050ce
Landon Waddell <Landon.waddell@gmail.com>
2021-09-05 20:22:11 -0600

    Removed comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,3 @@
-// const router = require("express").Router();
-// const path = require("path");
-
-// router.get("/exercise", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../public/exercise.html"));
-//   });
-  
-//   router.get("/stats", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../public/stats.html"));
-//   });
-
-
-
-// module.exports = router;
-
-
 const router = require("express").Router();
 const path = require('path');
 
